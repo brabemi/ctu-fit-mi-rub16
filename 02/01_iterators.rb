7b838d215e8776c905715f74fa486aa54225171e
Petr Pulc <pulcpetr@fit.cvut.cz>
2016-10-12 13:04:51 +0200

    Homework 02

---
+++ 02/01_iterators.rb
@@ -0,0 +1,9 @@
+#!/usr/bin/env ruby
+
+# Return array of odd elements from `array`
+# in case that that a block is given, yield
+# elements on odd index
+# e.g.: [1,2,3,4,5,6] -> [2,4,6]
+def odd_elements(array)
+  # @TODO write implementation
+end
