500ba204aaf6581e53696a91e53397bcb59199a7
Bc. Miroslav Brabenec <brabemi3@fit.cvut.cz>
2016-11-02 09:04:04 +0100

    VowelFinder and Summable

--- 04/vowel/vowel_finder.rb
+++ 04/vowel/vowel_finder.rb
@@ -1,6 +1,24 @@
 #!/usr/bin/env ruby
 
+require 'forwardable'
+
+module Summable
+  def sum
+    reduce(:+)
+  end
+end
+
+class Array
+  include Summable
+end
+
 # looks for vowels
 class VowelFinder
-
+  include Enumerable
+  include Summable
+  extend Forwardable
+  def_delegators :@proccesed_data, :each, :<<
+  def initialize(data)
+    @proccesed_data = data.downcase.split('').select { |c| c =~ /\A[aeiyou]/ }
+  end
 end
