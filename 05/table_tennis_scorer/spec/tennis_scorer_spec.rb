9c87f2e581b0ae0a052daf3b8ea62a2e6233c4f0
Bc. Miroslav Brabenec <brabemi3@fit.cvut.cz>
2016-11-12 13:51:20 +0100

    hw05

--- 05/table_tennis_scorer/spec/tennis_scorer_spec.rb
+++ 05/table_tennis_scorer/spec/tennis_scorer_spec.rb
@@ -3,56 +3,107 @@ require_relative 'spec_helper'
 require_relative '../table_tennis_scorer'
 
 describe TableTennisScorer do
-
   subject(:scorer) { TableTennisScorer.new }
 
   describe '#score'
-  
-    subject { scorer.score }
 
-    context 'at the beginning' do
-      it { is_expected.to eql '0-0' }
+  subject { scorer.score }
+
+  before(:each) do
+    scorer.reset
+  end
+
+  context 'at the beginning' do
+    it { is_expected.to eql '0-0' }
+  end
+
+  context 'server wins a point' do
+    it do
+      scorer.srv_score
+      is_expected.to eql '1-0'
     end
+  end
 
-    context 'server wins a point' do
-      before do
-        # ...
+  context 'receiver wins a point' do
+    it do
+      scorer.rec_score
+      is_expected.to eql '0-1'
+    end
+  end
+
+  context 'both win a point' do
+    it do
+      scorer.srv_score
+      scorer.rec_score
+      is_expected.to eql '1-1'
+    end
+  end
+
+  context 'server wins a game' do
+    # score of e.g. 10-7
+    # server scores
+    before do
+      10.times do
+        scorer.srv_score
       end
-      it { is_expected.to eql '1-0' }
+      7.times do
+        scorer.rec_score
+      end
+    end
+    it do
+      scorer.srv_score
+      is_expected.to eql 'server game'
+    end
+  end
+
+  context 'receiver wins a game' do
+    # score of e.g. 7-10
+    # receiver scores
+    before do
+      7.times do
+        scorer.srv_score
+      end
+      10.times do
+        scorer.rec_score
+      end
+    end
+    it do
+      scorer.rec_score
+      is_expected.to eql 'receiver game'
+    end
+  end
+
+  context 'two point lead after deuce' do
+    # game up to 10-10
+    before :all do
+      @scorer = TableTennisScorer.new
+      10.times do
+        @scorer.srv_score
+      end
+      10.times do
+        @scorer.rec_score
+      end
     end
 
-    context 'receiver wins a point' do
-      it { is_expected.to eql '0-1' }
-    end
-
-    context 'both win a point' do
-      it { is_expected.to eql '1-1' }
-    end
-
-    # ...
-    
-    context 'server wins a game' do
-      # score of e.g. 10-7
+    it do
       # server scores
-      it { is_expected.to eql 'server game' }
+      @scorer.srv_score
+      expect(@scorer.score).to eq '11-10'
     end
-    
-    context 'receiver wins a game' do
-      # score of e.g. 7-10
+    it do
       # receiver scores
-      it { is_expected.to eql 'receiver game' }
+      @scorer.rec_score
+      expect(@scorer.score).to eq '11-11'
     end
-    
-    context 'two point lead after deuce' do
-      # game up to 10-10
+    it do
       # server scores
-      it { is_expected.to eql '11-10' }
-      # receiver scores
-      it { is_expected.to eql '11-11' }
-      # server scores
-      it { is_expected.to eql '12-11' }
-      # server scores
-      it { is_expected.to eql 'server game' }
+      @scorer.srv_score
+      expect(@scorer.score).to eq '12-11'
     end
-    
+    it do
+      # server scores
+      @scorer.srv_score
+      expect(@scorer.score).to eq 'server game'
+    end
+  end
 end
--- 05/table_tennis_scorer/table_tennis_scorer.rb
+++ 05/table_tennis_scorer/table_tennis_scorer.rb
@@ -1,9 +1,30 @@
 # -*- encoding : utf-8 -*-
 class TableTennisScorer
-
-  # FIXME
-  def score
-    '0-0'
+  def initialize
+    @srv_score = 0
+    @rec_score = 0
   end
 
+  def score
+    if (@srv_score < 11 && @rec_score < 11) || (@srv_score - @rec_score).abs < 2
+      "#{@srv_score}-#{@rec_score}"
+    elsif @srv_score > @rec_score
+      'server game'
+    else
+      'receiver game'
+    end
+  end
+
+  def srv_score
+    @srv_score += 1
+  end
+
+  def rec_score
+    @rec_score += 1
+  end
+
+  def reset
+    @srv_score = 0
+    @rec_score = 0
+  end
 end
