152c499012d32ba084c5a32ab5051b763008ebde
Bc. Miroslav Brabenec <brabemi3@fit.cvut.cz>
2016-12-08 01:48:45 +0100

    Knuth's Algorithm X no repair

--- hw2/sudoku.rb
+++ hw2/sudoku.rb
@@ -22,81 +22,15 @@ class Sudoku
     !@grid.nil? && @grid.missing == 0
   end
 
-  def algorithm_X
-    dimension = @grid.dimension
-    block_width = @grid.block_width
-    nodes = Hash.new()
-    dimension.times do |i|
-      dimension.times { |r| nodes[{r: r, v: i+1}] = Set.new }
-      dimension.times { |c| nodes[{c: c, v: i+1}] = Set.new }
-      dimension.times { |b| nodes[{b: b, v: i+1}] = Set.new }
-    end
-    options = Set.new
-    dimension.times do |r|
-      dimension.times do |c|
-        if @grid.filled?(r,c) == false
-          dimension.times do |i|
-            b = r - r%block_width + c/block_width
-            options.add({r: r, c: c, b: b, v: i+1})
-            # p({r: r, c: c, b: b, v: i+1}.to_s)
-            nodes[{r: r, v: i+1}].add({r: r, c: c, b: b, v: i+1})
-            nodes[{c: c, v: i+1}].add({r: r, c: c, b: b, v: i+1})
-            nodes[{b: b, v: i+1}].add({r: r, c: c, b: b, v: i+1})
-          end
-        end
-      end
-    end
-
-    dimension.times do |r|
-      dimension.times do |c|
-        if @grid.filled?(r,c)
-          i = @grid.value(r,c)
-          b = r - r%block_width + c/block_width
-          colisions = nodes[{r: r, v: i}].flatten.merge(nodes[{c: c, v: i}]).merge(nodes[{b: b, v: i}])
-          # p colisions
-          colisions.each do |e|
-            nodes[{r: e[:r], v: e[:v]}].delete(e)
-            nodes[{c: e[:c], v: e[:v]}].delete(e)
-            nodes[{b: e[:b], v: e[:v]}].delete(e)
-          end
-        end
-      end
-    end
-
-  print @grid.to_s
-  node = nodes.min_by {|e| e[1].size == 0 ? 81 : e[1].size }
-  steps = 30
-  while node[1].size == 1 and steps > 0 do
-    my_sol = node[1].to_a[0]
-    # p my_sol
-    r = my_sol[:r]
-    c = my_sol[:c]
-    b = my_sol[:b]
-    i = my_sol[:v]
-    colisions = nodes[{r: r, v: i}].flatten.merge(nodes[{c: c, v: i}]).merge(nodes[{b: b, v: i}])
-    # p colisions
-    colisions.each do |e|
-      nodes[{r: e[:r], v: e[:v]}].delete(e)
-      nodes[{c: e[:c], v: e[:v]}].delete(e)
-      nodes[{b: e[:b], v: e[:v]}].delete(e)
-    end
-    @grid[r,c] = i
-    steps -= 1
-    node = nodes.min_by {|e| e[1].size == 0 ? 81 : e[1].size }
-  end
-  p nodes
-
-  print @grid.to_s
-  # print nodes
-  # p nodes.min_by {|e| e[1].size == 0 ? 81 : e[1].size }
-
-  end
-
   # Solves sudoku and returns 2D Grid
   def solve
     algorithm_X
     fail 'invalid game given' unless @grid.valid?
     puts "missing values #{@grid.missing}, filled #{@grid.filled}"
+  end
+
+  def solution
+     @grid.solution
   end
 
   protected
@@ -107,4 +41,60 @@ class Sudoku
     end
     fail "input '#{game}' is not supported yet"
   end
+
+  def algorithm_X
+    dimension = @grid.dimension
+    block_width = @grid.block_width
+    nodes = Hash.new()
+
+    dimension.times do |r|
+      dimension.times do |c|
+        nodes[{r: r, c: c}] = Set.new
+        dimension.times {|i| nodes[{r: r, c: c}].add(i+1)} if @grid.filled?(r,c) == false
+      end
+    end
+
+    dimension.times do |r|
+      dimension.times do |c|
+        if @grid.filled?(r,c)
+          i = @grid.value(r,c)
+          rs = r - r%block_width
+          cs = c - c%block_width
+          colisions = []
+          dimension.times {|r1| colisions.push({r: r1, c: c})}
+          dimension.times {|c1| colisions.push({r: r, c: c1})}
+          (rs...rs+block_width).each do |r1|
+            (cs...cs+block_width).each {|c1| colisions.push({r: r1, c: c1})}
+          end
+          colisions.each {|k| nodes[k].delete(i)}
+        end
+      end
+    end
+
+    node = nodes.min_by {|e| e[1].size == 0 ? 81 : e[1].size }
+    # steps = 81
+    # while node[1].size == 1 and steps > 0 do
+    while node[1].size == 1 do
+      i = node[1].to_a[0]
+      r = node[0][:r]
+      c = node[0][:c]
+      rs = r - r%block_width
+      cs = c - c%block_width
+      colisions = []
+      dimension.times {|r1| colisions.push({r: r1, c: c})}
+      dimension.times {|c1| colisions.push({r: r, c: c1})}
+      (rs...rs+block_width).each do |r1|
+        (cs...cs+block_width).each {|c1| colisions.push({r: r1, c: c1})}
+      end
+      colisions.each {|k| nodes[k].delete(i)}
+      @grid[r,c] = i
+      # steps -= 1
+      node = nodes.min_by {|e| e[1].size == 0 ? 81 : e[1].size }
+    end
+    # p nodes
+    # print @grid.to_s
+    # print nodes
+    # p nodes.min_by {|e| e[1].size == 0 ? 81 : e[1].size }
+  end
+
 end
