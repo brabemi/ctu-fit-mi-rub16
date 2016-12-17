837886ad2b86ea233ededf11af66c6993f09897b
Bc. Miroslav Brabenec <brabemi3@fit.cvut.cz>
2016-12-18 00:18:27 +0100

    collision and run away

--- 12/bird.rb
+++ 12/bird.rb
@@ -1,6 +1,7 @@
 class Bird
   def initialize(x, y, pig, score)
     @x, @y = x, y
+    @x_i, @y_i = x, y
     @img = Gosu::Image.new('media/bird.png')
     @pig = pig
     @hidden = false
@@ -8,12 +9,26 @@ class Bird
   end
 
   def draw
-    @img.draw(@x, @y, 1) unless @hidden
+    @img.draw(@x_i, @y_i, 1) unless @hidden
   end
 
   def update_delta(delta)
-    if @pig.x >= @x && @pig.y >= @y && @pig.x < (@x + 70) && @pig.y < (@y + 82)
-      hide()
+    unless @hidden
+      x_delta = ((@x + 34) - (@pig.x + 63))
+      y_delta = ((@y + 50) - (@pig.y + 99))
+      len = Math.sqrt(x_delta**2 + y_delta**2)
+      if len < 100
+        x_run = (x_delta / len) * delta * 90
+        y_run = (y_delta / len) * delta * 90
+        if ((x_delta + x_run.abs)**2 + (y_delta + y_run.abs)**2) < 7100
+          hide()
+        else
+          @x += x_run
+          @y += y_run
+          @x_i = @x.to_i
+          @y_i = @y.to_i
+        end
+      end
     end
   end
 
